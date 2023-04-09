3c725ffc34068125192592a6aff0aeb5e4797da0
Andreybukh2 <121665325+Andreybukh2@users.noreply.github.com>
2023-04-09 03:39:07 +0300

    Lesson13_Tests

--- src/test/java/Abstracts/getSquareTest.java
+++ src/test/java/Abstracts/getSquareTest.java
@@ -5,19 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class getSquareTest {
     @org.junit.jupiter.api.Test
     void getSquare() {
-       Triangle triangle = new Triangle(5,4,3,"Color");
+        Triangle triangle = new Triangle(5,4,3,"Color");
         assertEquals(6.0, triangle.getSquare());
         assertEquals("Color", triangle.getColor());
-        assertEquals("ТРЕУГОЛЬНИК", triangle.getName());
+        assertEquals("TRIANGLE", triangle.getName());
 
         Rectangle rectangle = new Rectangle(5,4,"Color");
         assertEquals(20.0, rectangle.getSquare());
         assertEquals("Color", rectangle.getColor());
-        assertEquals("ПРЯМОУГОЛЬНИК", rectangle.getName());
+        assertEquals("RECTANGLE", rectangle.getName());
 
         Circle circle = new Circle(5,"Color");
         assertEquals(78.53750000000001, circle.getSquare());
         assertEquals("Color", circle.getColor());
-        assertEquals("КРУГ", circle.getName());
+        assertEquals("CIRCLE", circle.getName());
     }
 }
